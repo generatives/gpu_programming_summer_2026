6e8ce5d49c39803d9a73c8440e72b52de1202745
Declan Easton <declaneaston@gmail.com>
2026-07-08 19:43:22 -0700

    Do init inside the kernel (made performance worse)

--- cliff_gpu/kmeans/tiled_kmeans.py
+++ cliff_gpu/kmeans/tiled_kmeans.py
@@ -3,7 +3,9 @@ import time
 import numpy as np
 import warp as wp
 
-TILE_SIZE = 512
+wp.config.enable_backward = False
+
+BLOCK_SIZE = 512
 MAX_DATA_SIZE = 4096
 NUM_CLUSTERS = 3
 MAX_ITERATIONS = 100
@@ -12,6 +14,19 @@ PROGRESS_TOLERANCE = 1e-4
 @wp.func
 def sq_length(v: wp.vec2f):
     return wp.dot(v, v)
+
+@wp.func
+def index_if_above(val: float, idx: int, threshold: float):
+    if val > threshold:
+        return idx
+    return 0x7FFFFFFF   # sentinel: larger than any real index
+
+@wp.func
+def first_idx_above(data_tile: wp.tile[wp.float32, MAX_DATA_SIZE], threshold: float):
+    idx_tile = wp.tile_arange(MAX_DATA_SIZE, dtype=int)
+    candidate_indices = wp.tile_map(index_if_above, data_tile, idx_tile, threshold)
+    first_idx = wp.tile_min(candidate_indices)   # smallest index where condition held, or sentinel if none did
+    return first_idx
 
 @wp.kernel
 def kmean_iteration(X: wp.array[wp.vec2f],
@@ -31,14 +46,42 @@ def kmean_iteration(X: wp.array[wp.vec2f],
     model_centroids = wp.tile_load(centroids, shape=(NUM_CLUSTERS,), offset=(model_offset,))
 
     data_tile = wp.tile_empty((MAX_DATA_SIZE,), dtype=wp.vec2f)
-    label_tile = wp.tile_empty((MAX_DATA_SIZE,), dtype=wp.int32)
+    label_tile = wp.tile_empty((MAX_DATA_SIZE,), dtype=wp.int8)
 
     # load the needed data into shared memory
     tile_data_idx = i
     while tile_data_idx < MAX_DATA_SIZE:
         buffer_data_idx = tile_data_idx + data_start
         data_tile[tile_data_idx] = X[buffer_data_idx] if buffer_data_idx < data_end else wp.vec2f(0.0, 0.0)
-        tile_data_idx = tile_data_idx + TILE_SIZE
+        tile_data_idx = tile_data_idx + BLOCK_SIZE
+
+    # init centroids
+    state = wp.rand_init(0)
+    model_centroids = wp.tile_empty((NUM_CLUSTERS,), dtype=wp.vec2f)
+    min_distances = wp.tile_full((MAX_DATA_SIZE,), wp.inf, dtype=wp.float16)
+    for i in range(0, NUM_CLUSTERS):
+        if i == 0:
+            # First centroid: random selection
+            first_idx = wp.randi(state, 0, actual_data_size)
+            new_centroid = data_tile[first_idx]
+        else:
+            weights = min_distances / wp.tile_sum(min_distances)[0]
+            cpf = wp.tile_scan_inclusive(wp.tile_astype(weights, dtype=wp.float32))
+            selected = wp.randf(state)
+            selected_idx = first_idx_above(cpf, selected)
+            new_centroid = data_tile[selected_idx[0]]
+        
+        model_centroids[i] = new_centroid
+
+        differences = wp.tile_map(wp.sub, data_tile, new_centroid)
+        distances = wp.tile_astype(wp.tile_map(wp.length_sq, differences), dtype=wp.float16)
+        min_distances = wp.tile_map(wp.min, min_distances, distances)
+
+        tile_data_idx = i
+        while tile_data_idx < MAX_DATA_SIZE:
+            min_distances[tile_data_idx] = wp.float16(0.0) if tile_data_idx >= actual_data_size else min_distances[tile_data_idx]
+            tile_data_idx = tile_data_idx + BLOCK_SIZE
+
 
     iteration = int(0)
     while iteration < MAX_ITERATIONS:
@@ -55,7 +98,7 @@ def kmean_iteration(X: wp.array[wp.vec2f],
                     label = j
             label_tile[tile_data_idx] = label
             
-            tile_data_idx = tile_data_idx + TILE_SIZE
+            tile_data_idx = tile_data_idx + BLOCK_SIZE
 
         # accumulate updated centroids and counts
         new_model_centroids = wp.tile_zeros((NUM_CLUSTERS,), dtype=wp.vec2f)
@@ -63,12 +106,12 @@ def kmean_iteration(X: wp.array[wp.vec2f],
 
         tile_data_idx = i
         while tile_data_idx < MAX_DATA_SIZE:
-            label = label_tile[tile_data_idx]
+            label = wp.int32(label_tile[tile_data_idx])
             data = data_tile[tile_data_idx]
             wp.tile_scatter_add(new_model_centroids, label, data, has_value=tile_data_idx<actual_data_size, atomic=True)
             wp.tile_scatter_add(new_model_counts, label, 1.0, has_value=tile_data_idx<actual_data_size, atomic=True)
             
-            tile_data_idx = tile_data_idx + TILE_SIZE
+            tile_data_idx = tile_data_idx + BLOCK_SIZE
 
         # normalize centroids
         new_model_centroids = new_model_centroids / new_model_counts
@@ -140,10 +183,12 @@ class TiledKMeans:
         assert X.shape[1] == 2, "Input data must have 2 features (2D points)"
         assert model_idx_lookup.shape[0] == X.shape[0], "model_idx_lookup should have the same length as the dataset"
 
-        start_time = time.time()
-        self._kinit(X, offsets)
-        end_time = time.time()
-        print(f"Init took: {end_time - start_time}s")
+        #start_time = time.time()
+        #self._kinit(X, offsets)
+        num_centroids = self.n_models * self.n_clusters
+        self.centroids = np.empty((num_centroids, X.shape[1]))
+        #end_time = time.time()
+        #print(f"Init took: {end_time - start_time}s")
         self.labels = []
 
         x_buffer = wp.array(X, dtype=wp.vec2f, device="cuda")
@@ -153,6 +198,6 @@ class TiledKMeans:
         wp.launch_tiled(kmean_iteration,
                         dim=self.n_models,
                         inputs=[x_buffer, centroids_buffer, offsets_buffer, self.n_clusters],
-                        block_dim=TILE_SIZE)
+                        block_dim=BLOCK_SIZE)
 
         self.centroids = centroids_buffer.numpy()
